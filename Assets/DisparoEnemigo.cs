f7c717d1ceff3b5fead0a4a6897974d44b5a7b65
maite <maitevicky@gmail.com>
2026-08-29 11:05:45 -0300

    +agregue codigo de disparo de enemigo

---
+++ Assets/DisparoEnemigo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DisparoEnemigo : MonoBehaviour
+{
+
+    public Transform controladorDisparo;
+    public float distanciaLinea;
+
+    public LayerMask capaJugador;
+
+    public bool jugadorEnRango;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
