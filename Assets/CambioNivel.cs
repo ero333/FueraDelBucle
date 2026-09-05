4511ed2989829af6a89ed836eac6aecdca11c62f
maite <maitevicky@gmail.com>
2026-09-05 11:18:10 -0300

    +agregue boton del siguiente nivel cuando el jugador toca la puerta y coloque las plataformas basica y acomode a los repeaters en el nivel 1

--- Assets/CambioNivel.cs
+++ Assets/CambioNivel.cs
@@ -1,20 +1,57 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CambioNivel : MonoBehaviour
 {
-    [Header("Configuración")]
-    [Tooltip("Nivel2")]
+    [Header("Configuración de Escena")]
+    [Tooltip("Nombre de la escena a la que se cambiará.")]
     public string nombreEscenaSiguiente;
 
-    [Tooltip("Tag que debe tener el jugador para activar el cambio de nivel.")]
+    [Tooltip("Tag que debe tener el jugador para activar la interacción.")]
     public string tagJugador = "Player";
+
+    [Header("Interfaz de Usuario")]
+    [Tooltip("Arrastra aquí el Botón de la UI desde la jerarquía.")]
+    public Button botonCambioEscena;
+
+    private void Start()
+    {
+        
+        if (botonCambioEscena != null)
+        {
+            botonCambioEscena.gameObject.SetActive(false);
+
+            
+            botonCambioEscena.onClick.AddListener(CargarSiguienteEscena);
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado el botón en el Inspector de CambioNivel.");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag(tagJugador))
+       
+        if (other.CompareTag(tagJugador) && botonCambioEscena != null)
         {
-            SceneManager.LoadScene(nombreEscenaSiguiente);
+            botonCambioEscena.gameObject.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       
+        if (other.CompareTag(tagJugador) && botonCambioEscena != null)
+        {
+            botonCambioEscena.gameObject.SetActive(false);
+        }
+    }
+
+    
+    public void CargarSiguienteEscena()
+    {
+        SceneManager.LoadScene(nombreEscenaSiguiente);
     }
 }
